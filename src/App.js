8aea189f12217950d212329514f635a3ebad1d39
jiyoung-lee <jno0717@naver.com>
2020-03-05 15:34:06 +0900

    SVG로 barChart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import SingleBar from "./components/barChart";
+
+const max = 300;
 
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    genList();
+  }, []);
+
+  const genList = () => {
+    const list = [];
+    for (let i = 0; i < 5; i++) {
+      list[i] = Math.random() * 101;
+    }
+    setList(list);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div style={{ display: "flex" }}>
+        {list.map(e => {
+          const y = max - (max * e) / 100;
+          return (
+            <SingleBar
+              key={e}
+              width="60px"
+              height="300px"
+              color="#ea1"
+              percentage={`${Number(e).toFixed(2)} %`}
+              data={`M 0 ${max} L 0  ${y} L 50 ${y} L 50 ${max} Z`}
+            />
+          );
+        })}
+      </div>
+      <button onClick={genList}>Generate</button>
     </div>
   );
 }
